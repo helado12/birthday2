999a352da104f380e4b56716e0454cdccf52f111
hhe233 <58110129+hhe233@users.noreply.github.com>
2020-05-17 23:06:56 +0100

    a

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ document.addEventListener("touchstart", function(e) {
 
 // Particle Object
 var Particle = function() {
-  this.w = Math.random() * 7.5;
-  this.h = Math.random() * 7.5;
+  this.w = Math.random() * 6;
+  this.h = Math.random() * 6;
   this.x = -W;
   this.y = -H;
   this.free = false;
@@ -111,8 +111,8 @@ function positionParticles() {
   data = imageData.data;
   
   // Iterate each row and column
-  for (var i = 0; i < imageData.height*5; i += density) {
-    for (var j = 0; j < imageData.width*5; j += density) {
+  for (var i = 0; i < imageData.height*3; i += density) {
+    for (var j = 0; j < imageData.width*3; j += density) {
       
       // Get the color of the pixel
       var color = data[((j * ( imageData.width * 4)) + (i * 4)) - 1];
